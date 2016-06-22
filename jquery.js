2d70330951d419b67089af8c969482cf4398c3d1
ramonavic <ramon.hofmeister@gmail.com>
2016-06-22 10:44:17 +0200

    Add and clean up todos

--- jquery.js
+++ jquery.js
@@ -7,7 +7,6 @@ function updateCounters() {
   $("#todo-count").html(ntodos - ncompleted);
 }
 
-updateCounters ();
 
 function toggleDone () {
   var checkbox = this;
@@ -18,4 +17,63 @@ function toggleDone () {
 
 }
 
+function createTodo(title) {
+  var checkboxId = "todo-" + nextTodoId();
+
+  var listItem = $("<li></li>");
+  listItem.addClass("todo");
+
+  var checkbox = $('<input>');
+  checkbox.attr('type', 'checkbox');
+  checkbox.attr('id', checkboxId);
+  checkbox.val(1);
+  checkbox.bind('change', toggleDone);
+
+  var space = document.createTextNode(" ");
+
+  var label = $('<label></label>');
+  label.attr('for', checkboxId);
+  label.html(title);
+
+  listItem.append(checkbox);
+  listItem.append(space);
+  listItem.append(label);
+
+  $("#todolist").append( listItem );
+
+  updateCounters();
+}
+
+function nextTodoId() {
+  return $(".todo").length + 1;
+}
+
+
+
+function submitTodo(event) {
+  event.preventDefault();
+
+  var title = $("#new-todo").val()
+
+  createTodo(title);
+
+  $("new-todo").val(null)
+
+  updateCounters();
+}
+
+function cleanUpDoneTodos (event) {
+  event.preventDefault();
+  $.when($(".completed").remove())
+    .then(updateCounters);
+}
+
+
+
 $("input[type=checkbox]").bind('change', toggleDone);
+
+$("form").bind('submit', submitTodo);
+
+$("#clean-up").bind('click', cleanUpDoneTodos);
+
+updateCounters ();
